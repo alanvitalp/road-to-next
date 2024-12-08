4da637c603d4069c34d2e5ef2baaa8d6d3c4c779
Alan <alanvital18@gmail.com>
2024-12-08 20:01:19 -0300

    chore: init shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import Link from "next/link";
+import { ticketsPath } from "@/path";
+
+export default function HomePage() {
   return (
-    <h1>Hello page!</h1>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to Tickets
+        </Link>
+      </div>
+    </div>
   );
 }
