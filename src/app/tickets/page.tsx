69f8785b99b262dc4af300c3a6a93b11e2d49cc0
Alan <alanvital18@gmail.com>
2025-03-25 20:39:43 -0300

    feat: create new ticket

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,12 +3,24 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Heading } from "@/components/heading";
 import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { CreateTicketForm } from "@/features/ticket/components/create-ticket-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title={"Tickets"} description={"All your tickets at one place"}/>
+
+      <Card className="w-full max-w-[420px] self-center">
+        <CardHeader>
+          <CardTitle>Create Ticket</CardTitle>
+          <CardDescription>A new ticket will be created</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <CreateTicketForm />
+        </CardContent>
+      </Card>
 
       <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
         <Suspense fallback={<Spinner />}>
