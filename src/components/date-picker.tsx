0aaa3b8da70461f72b9addb24692cd3f41298228
Alan <alanvital18@gmail.com>
2025-03-27 22:02:23 -0300

    feat: reset date picker

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -2,7 +2,7 @@
 
 import { format } from "date-fns"
 import {  LucideCalendar } from "lucide-react"
-import { useState } from "react"
+import React, { useImperativeHandle, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import {
@@ -11,34 +11,50 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
+export type ImperativeHandleFromDatePicker = {
+  reset: () => void
+}
+
 type DatePickerProps = {
   id: string
   name: string
   defaultValue?: string | undefined
+  imperativeHandleRef: React.RefObject<ImperativeHandleFromDatePicker>
 }
 
-const DatePicker = ({ id, name, defaultValue }: DatePickerProps) => {
+const DatePicker = ({ id, name, defaultValue, imperativeHandleRef }: DatePickerProps) => {
   const [date, setDate] = useState<Date | undefined>(defaultValue ? new Date(defaultValue) : new Date())
 
-  const formattedStringDate = date ? format(date, "yyyy-MM-dd") : ""
+  const formattedStringDate = date ? format(date, "yyyy-MM-dd") : "";
+
+  useImperativeHandle(imperativeHandleRef, () => ({
+    reset: () => setDate(new Date())
+  }))
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleSelect = (selectedDate: Date | undefined) => {
+    setDate(selectedDate);
+    setOpen(false)
+  }
 
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger className="w-full" id={id} asChild>
-        <input type="hidden" name={name} value={formattedStringDate} />
         <Button
           variant={"outline"}
           className="justify-start font-normal text-left"
         >
-          <LucideCalendar className="w-4 h-4 mr-2"/>
+          <LucideCalendar className="w-4 h-4 mr-2" />
           {formattedStringDate}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
+      <input type="hidden" name={name} value={formattedStringDate} />
         <Calendar
           mode="single"
           selected={date}
-          onSelect={setDate}
+          onSelect={handleSelect}
           initialFocus
         />
       </PopoverContent>
