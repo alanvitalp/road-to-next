5646a102515958006da45999f058da38c728a400
Alan <alanvital18@gmail.com>
2025-05-05 23:27:38 -0300

    feat: adding useQueryStates to sort by key

--- src/features/ticket/types.ts
+++ src/features/ticket/types.ts
@@ -9,19 +9,23 @@ export type TicketWithMetadata = Prisma.TicketGetPayload<{
   };
 }>;
 
-export const searchParser = parseAsString.withDefault("").withOptions({
+export const sortOptions = {
   shallow: false,
   clearOnDefault: true,
+};
+
+export const searchParser = parseAsString.withDefault("").withOptions({
+  ...sortOptions
 });
 
-export const sortParser = parseAsString.withDefault("newest").withOptions({
-  shallow: false,
-  clearOnDefault: true,
-});
+export const sortParser = {
+  sortKey: parseAsString.withDefault("createdAt"),
+  sortValue: parseAsString.withDefault("desc"),
+};
 
 export const searchParamsCache = createSearchParamsCache({
   search: searchParser,
-  sort: sortParser,
+  ...sortParser,
 });
 
 export type ParsedSearchParams = Awaited<
