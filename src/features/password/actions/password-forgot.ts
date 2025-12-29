12ce98d9f4da91f6db956d716e4e69c0c01fdf2c
Alan <alanvital18@gmail.com>
2025-12-29 17:42:25 -0300

    feat: send email to reset password

--- src/features/password/actions/password-forgot.ts
+++ src/features/password/actions/password-forgot.ts
@@ -8,6 +8,7 @@ import {
 } from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { generatePasswordResetLink } from "../utils/generate-password-reset-link";
+import { sendEmailPasswordReset } from "../emails/send-email-password-reset";
 
 const passwordForgotSchema = z.object({
   email: z.string().min(1, { message: "Is required" }).max(191).email(),
@@ -32,9 +33,7 @@ export const passwordForgot = async (
 
     const passwordResetLink = await generatePasswordResetLink(user.id);
 
-    // TODO: Send email with reset link
-    // instead we will just print it to the console for now
-    console.log(passwordResetLink);
+    await sendEmailPasswordReset(user.username, user.email, passwordResetLink);
   } catch (error) {
     return fromErrorToActionState(error, formData);
   }
